d7bc695d58e1cb7c36576f93001b9471bb3a6b98
Bryan-Cee <cheruiyotbryan@gmail.com>
2019-11-10 17:10:13 +0300

    queue algorithm with dynamic lists

---
+++ python/queues.py
@@ -0,0 +1,86 @@
+import unittest
+
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+
+    def add_node(self, value):
+        self.head = Node(value)
+
+
+class QueueTwoStacks(object):
+
+    def __init__(self):
+        self.items = []
+    # Implement the enqueue and dequeue methods
+
+    def enqueue(self, item):
+        return self.items.append(item)
+
+    def dequeue(self):
+        if len(self.items) == 0:
+            raise Exception('The queue is empty')
+        return self.items.pop(0)
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_basic_queue_operations(self):
+        queue = QueueTwoStacks()
+
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
+
+        actual = queue.dequeue()
+        expected = 1
+        self.assertEqual(actual, expected)
+
+        actual = queue.dequeue()
+        expected = 2
+        self.assertEqual(actual, expected)
+
+        queue.enqueue(4)
+
+        actual = queue.dequeue()
+        expected = 3
+        self.assertEqual(actual, expected)
+
+        actual = queue.dequeue()
+        expected = 4
+        self.assertEqual(actual, expected)
+
+    def test_error_when_dequeue_from_new_queue(self):
+        queue = QueueTwoStacks()
+
+        with self.assertRaises(Exception):
+            queue.dequeue()
+
+    def test_error_when_dequeue_from_empty_queue(self):
+        queue = QueueTwoStacks()
+
+        queue.enqueue(1)
+        queue.enqueue(2)
+
+        actual = queue.dequeue()
+        expected = 1
+        self.assertEqual(actual, expected)
+
+        actual = queue.dequeue()
+        expected = 2
+        self.assertEqual(actual, expected)
+
+        with self.assertRaises(Exception):
+            queue.dequeue()
+
+
+unittest.main(verbosity=2)
